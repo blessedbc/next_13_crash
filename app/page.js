db7cf55b3aaffe34adc1ad01c080143c889a88d5
ikenna nwaka <inwaka39@gmail.com>
2025-06-09 07:51:54 +0200

    HomePage

--- app/page.js
+++ app/page.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import Link from "next/link"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>welcome To My Page</h1>
+      <ul>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+         <li>
+          <Link href='/about'>About</Link>
+        </li>
+         <li>
+          <Link href='/about/team'>Team</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
