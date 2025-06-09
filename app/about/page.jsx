097db8bdf340b688b147597de4c08c5637451a4d
ikenna nwaka <inwaka39@gmail.com>
2025-06-09 07:59:08 +0200

    TeamPage

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div>
+      <h1>About My Page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique facere dolore molestiae esse, soluta hic, fugiat tempora placeat reprehenderit exercitationem voluptates consectetur unde? Perspiciatis recusandae distinctio a consequatur dignissimos amet magni, cupiditate eveniet molestias harum pariatur laborum, praesentium nihil sapiente dolorum. Delectus molestiae magnam consequatur quidem dolore temporibus facilis consectetur!
+      </p>
+    </div>
   )
 }
 
---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium nobis repellat consectetur explicabo. Amet voluptates rerum distinctio eum facilis earum doloribus culpa nesciunt cumque dicta perspiciatis, maiores laudantium nisi saepe!</p>
+    </div>
+  )
+}
+
+export default TeamPage
