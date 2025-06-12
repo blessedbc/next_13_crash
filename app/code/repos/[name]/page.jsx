87bf47916e751aab826e39daf766306429072e50
ikenna nwaka <inwaka39@gmail.com>
2025-06-12 06:40:47 +0200

    update

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,8 +1,9 @@
+import Repo from "@/app/components/Repo"
+
 const RepoPage = ({ params: { name } }) => {
   return (
     <div className='card'>
-      <h2>{name}</h2>
-      <p>Repo Details</p>
+      <Repo name={name} />
     </div>
   )
 }
