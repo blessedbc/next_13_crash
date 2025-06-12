93477551186c9ec27f2f2c7a0233b39b73381c13
ikenna nwaka <inwaka39@gmail.com>
2025-06-12 07:21:50 +0200

    Suspense Boundries

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import Link from "next/link"
 import Repo from "@/app/components/Repo"
 import RepoDirs from "@/app/components/RepoDirs"
@@ -5,9 +6,15 @@ import RepoDirs from "@/app/components/RepoDirs"
 const RepoPage = ({ params: { name } }) => {
   return (
     <div className='card'>
-      <Link href='/code/repos' className="btn btn-back">Back To Repositories</Link>
-      <Repo name={name} />
+      <Link href='/code/repos' className="btn btn-back">
+      Back To Repositories
+      </Link>
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading repo...</div>}>
       <RepoDirs name={name} />
+      </Suspense>
     </div>
   )
 }
